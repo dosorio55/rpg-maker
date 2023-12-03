b0e262fdfb3cd341611419263778a6877b91ed08
Diego Alejandro Osorio Ruiz <dosorio55@gmail.com>
2023-12-03 20:21:25 +0100

    trying to fix error on animator

--- Assets/Scripts/PlayerScripts/AnimationTriggers.cs
+++ Assets/Scripts/PlayerScripts/AnimationTriggers.cs
@@ -1,20 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class AnimationTrigger : MonoBehaviour
-{
-    private Player player;
+// public class AnimationTrigger : MonoBehaviour
+// {
+//     private Player player;
 
-    private void Awake()
-    {
-        player = GetComponentInParent<Player>();
-    }
+//     private void Start()
+//     {
+//         player = GetComponentInParent<Player>();
+//     }
 
-    public void AnimationFinishTrigger()
-    {
-        player.AnimationFinishTrigger();
-    }
+//     public void AnimationFinishTrigger()
+//     {
+//         player.AnimationFinishTrigger();
+//     }
 
 
-}
+// }
